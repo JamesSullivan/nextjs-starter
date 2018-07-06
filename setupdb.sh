f99c8aced3762875e3def6128ac958cdcc006908
James Sullivan <james.brian.sullivan@gmail.com>
2018-07-06 17:28:29 +0900

    seems to be working now

---
+++ setupdb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mysql -uroot -p <<MYSQL_SCRIPT
+CREATE DATABASE IF NOT EXISTS starter DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;
+CREATE USER 'starter_user'@'localhost' IDENTIFIED BY 'starterPW1!';
+GRANT ALL PRIVILEGES ON starter.* TO 'starter_user'@'localhost' IDENTIFIED BY 'starterPW1!';
+FLUSH PRIVILEGES;
+use starter;
+CREATE TABLE users (
+  id bigint(20) NOT NULL AUTO_INCREMENT, jdoc JSON, PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+MYSQL_SCRIPT
+
+echo "MySQL database starter created."
+echo "MySQL user starter_user created."
+echo "Username:   starter_user"
+echo "Password:   starterPW1!"
+
+
+echo "MySQL database starter tables created."
